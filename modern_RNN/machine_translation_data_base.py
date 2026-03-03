7ce7e1a577553eac4841564e4fe053c79b761190
dchpolymer <fengyinkaidashabi@gmail.com>
2026-03-03 16:36:36 +0800

    Initial commit

---
+++ modern_RNN/machine_translation_data_base.py
@@ -0,0 +1,60 @@
+import os
+import torch
+from d2l import torch as d2l
+
+
+#@save
+d2l.DATA_HUB['fra-eng'] = (d2l.DATA_URL + 'fra-eng.zip',
+                           '94646ad1522d915e7b0f9296181140edcf86a4f5')
+
+#@save
+def read_data_nmt():
+    """载入“英语－法语”数据集"""
+    data_dir = d2l.download_extract('fra-eng')
+    with open(os.path.join(data_dir, 'fra.txt'), 'r',
+             encoding='utf-8') as f:
+        return f.read()
+
+raw_text = read_data_nmt()
+# print(raw_text[:75])
+
+
+#@save
+def preprocess_nmt(text):
+    """预处理“英语－法语”数据集"""
+    def no_space(char, prev_char):
+        return char in set(',.!?') and prev_char != ' '
+
+    # 使用空格替换不间断空格
+    # 使用小写字母替换大写字母
+    text = text.replace('\u202f', ' ').replace('\xa0', ' ').lower()
+    # 在单词和标点符号之间插入空格
+    out = [' ' + char if i > 0 and no_space(char, text[i - 1]) else char
+           for i, char in enumerate(text)]
+    return ''.join(out)
+
+text = preprocess_nmt(raw_text)
+# print(text[:80])
+# for i,line in enumerate(text.split('\n')):
+#     print(i,line)
+#     break   # 0 go .	va !
+
+
+
+#@save
+def tokenize_nmt(text, num_examples=None):
+    """词元化“英语－法语”数据数据集"""
+    source, target = [], []
+    for i, line in enumerate(text.split('\n')):
+        if num_examples and i > num_examples:
+            break
+        parts = line.split('\t')
+        if len(parts) == 2:
+            source.append(parts[0].split(' '))
+            target.append(parts[1].split(' '))
+    return source, target
+
+source, target = tokenize_nmt(text)
+# print(len(source), len(target))
+# print(source[:10])
+# print(target[:10])
