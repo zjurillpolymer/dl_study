c78a4fba302e5be12f3f2a6da63bb3c7551d57de
dchpolymer <fengyinkaidashabi@gmail.com>
2026-03-10 20:54:15 +0800

    initial commit

---
+++ Preliminary optimization algorithm/learning_rate_scheduler_example.py
@@ -0,0 +1,105 @@
+import os
+# 必须在导入任何可能使用 OpenMP 的库之前设置
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+
+import math
+import torch
+from torch import nn
+from torch.optim import lr_scheduler
+from d2l import torch as d2l
+from matplotlib import pyplot as plt
+
+def net_fn():
+    model = nn.Sequential(
+        nn.Conv2d(1, 6, kernel_size=5, padding=2), nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        nn.Conv2d(6, 16, kernel_size=5), nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2),
+        nn.Flatten(),
+        nn.Linear(16 * 5 * 5, 120), nn.ReLU(),
+        nn.Linear(120, 84), nn.ReLU(),
+        nn.Linear(84, 10))
+
+    return model
+
+loss = nn.CrossEntropyLoss()
+device = d2l.try_gpu()
+
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
+
+# 代码几乎与d2l.train_ch6定义在卷积神经网络一章LeNet一节中的相同
+def train(net, train_iter, test_iter, num_epochs, loss, trainer, device,
+          scheduler=None):
+    net.to(device)
+    animator = d2l.Animator(xlabel='epoch', xlim=[0, num_epochs],
+                            legend=['train loss', 'train acc', 'test acc'])
+
+    for epoch in range(num_epochs):
+        metric = d2l.Accumulator(3)  # train_loss,train_acc,num_examples
+        for i, (X, y) in enumerate(train_iter):
+            net.train()
+            trainer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            l.backward()
+            trainer.step()
+            with torch.no_grad():
+                metric.add(l * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
+            train_loss = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i + 1) % 50 == 0:
+                animator.add(epoch + i / len(train_iter),
+                             (train_loss, train_acc, None))
+
+        test_acc = d2l.evaluate_accuracy_gpu(net, test_iter)
+        animator.add(epoch+1, (None, None, test_acc))
+
+        if scheduler:
+            if scheduler.__module__ == lr_scheduler.__name__:
+                # UsingPyTorchIn-Builtscheduler
+                scheduler.step()
+            else:
+                # Usingcustomdefinedscheduler
+                for param_group in trainer.param_groups:
+                    param_group['lr'] = scheduler(epoch)
+
+    print(f'train loss {train_loss:.3f}, train acc {train_acc:.3f}, '
+          f'test acc {test_acc:.3f}')
+
+
+lr, num_epochs = 0.3, 30
+net = net_fn()
+trainer = torch.optim.SGD(net.parameters(), lr=lr)
+train(net, train_iter, test_iter, num_epochs, loss, trainer, device)
+plt.show()
+
+
+
+### 余弦调度器
+class CosineScheduler:
+    def __init__(self,max_update,base_lr=0.01,final_lr=1.0,
+                 warmup_steps=0,warmup_begin_lr=0):
+        self.base_lr_orig = base_lr
+        self.max_update = max_update
+        self.final_lr = final_lr
+        self.warmup_steps = warmup_steps
+        self.warmup_begin_lr = warmup_begin_lr
+        self.max_steps = self.max_update - self.warmup_steps
+
+
+    def get_warmup_lr(self,epoch):
+        increase=(self.base_lr_orig-self.warmup_begin_lr)*float(epoch)/float(self.warmup_steps)
+        return self.warmup_begin_lr+increase
+
+    def __call__(self,epoch):
+        if epoch < self.warmup_steps:
+            return self.get_warmup_lr(epoch)
+
+        if epoch<=self.max_update:
+            self.base_lr = self.final_lr + (
+                    self.base_lr_orig - self.final_lr) * (1 + math.cos(
+                math.pi * (epoch - self.warmup_steps) / self.max_steps)) / 2
+        return self.base_lr
