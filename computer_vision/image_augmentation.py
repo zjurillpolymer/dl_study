72ec87126321121ba0f03682f4a41a7efa595118
zjurillpolymer <dingchenhangzju@outlook.com>
2026-05-06 20:09:42 +0800

    macos第一次提交

---
+++ computer_vision/image_augmentation.py
@@ -0,0 +1,11 @@
+import torch
+import torchvision
+from torch import nn
+from d2l import torch as d2l
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+img = Image.open('/Users/arcadio/PycharmProjects/dl_study/computer_vision/img.png')
+plt.imshow(img)
+plt.show()
